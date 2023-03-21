7fbfa4c25dce1feb2cc0be6716254a8c2c033ef9
sagor369 <sahedsalma369@gmail.com>
2023-03-21 22:24:25 +0600

    frast react projects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+const friends = [
+  {name:'sagor ' , job: 'electrical'},
+  {name:'hasan ' , job: 'mechanical'},
+  {name:'anamul ' , job: 'civil'},
+  {name:'shahin ' , job: 'etb'},
+
+]
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counts></Counts>
+      {
+          friends.map(friend => <Parson name={friend.name} job={friend.job}></Parson>)
+      }
+      
+        <UserLoad></UserLoad>
+      
     </div>
   );
+}
+function UserLoad (){
+  const [users, setUsers] = useState([])
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  return(
+    <div className='friend-list'> 
+      {
+        users.map(user => <UserShow name={user.name} email={user.email}></UserShow>)
+      }
+    </div>
+    
+  )
+}
+function UserShow(props){
+  return(
+    <div className='parson-list'>
+      <h3> Name : {props.name}</h3>
+      <p>Email : {props.email}</p>
+    </div>
+  )
+}
+function Counts(){
+  const [count, setCount] = useState(55)
+  const CountPlas = ()=>(setCount(count+1))
+  const CountMinus = () => (setCount(count -1))
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={CountPlas}>Add</button>
+      <button onClick={CountMinus}>CountMinus</button>
+    </div>
+  )
+}
+function Parson(props){
+  return(
+    <div className='parson-list'>
+      <h1>My name is : {props.name} </h1>
+      <p>my work is {props.job} </p>
+    </div>
+  )
 }
 
 export default App;
